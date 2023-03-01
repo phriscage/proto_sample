68f3e01662bb60b0617cda854128175d46152bf3
Chris Page <phriscage@gmail.com>
2023-03-01 10:18:52 -0500

    chaning datetime fields to _at

--- db/migrations/postgresql/000003_person_phone_numbers.up.sql
+++ db/migrations/postgresql/000003_person_phone_numbers.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS `person_phone_numbers` (
 	`id` VARCHAR(36) NOT NULL PRIMARY KEY,
 	`person_id` VARCHAR(36) NOT NULL,
 	`phone_number_id` VARCHAR(36) NOT NULL,
-	`created_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	`created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
